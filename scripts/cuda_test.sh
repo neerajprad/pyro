aae04de698fa0d2aef108b117efb904706244483
Neeraj Pradhan <npradhan@uber.com>
2018-05-17 18:47:01 -0700

    fixes to scripts

--- scripts/cuda_test.sh
+++ scripts/cuda_test.sh
@@ -3,12 +3,28 @@
 set -xe
 
 BRANCH_NAME=cron-jobs
-VIRTUALENV=pyro-cron-27
 
+# On exit, go back to `dev` branch and remove `cron-jobs` branch
+function _cleanup() {
+  [[ ${#DIRSTACK[@]} -gt 1 ]] && popd
+  git rev-parse --verify "${BRANCH_NAME}"
+  git checkout dev
+  if [[ $? == 0 ]]; then
+    git branch -D "${BRANCH_NAME}"
+  fi
+}
+
+trap _cleanup EXIT
+
+VIRTUALENV=pyro-cron-27
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${DIR}"
 source activate "${VIRTUALENV}"
 git checkout dev
 git pull upstream dev
+git fetch origin "${BRANCH_NAME}"
 git checkout "${BRANCH_NAME}"
-git pull origin "${BRANCH_NAME}"
 git rebase dev
+
 make test-cuda
--- scripts/perf_cron.sh
+++ scripts/perf_cron.sh
@@ -2,16 +2,31 @@
 
 set -xe
 
-REF_HEAD_FILE=.cron/ref_head.txt
 BRANCH_NAME=cron-jobs
-VIRTUALENV=pyro-cron-27
 
+# On exit, go back to `dev` branch and remove `cron-jobs` branch
+function _cleanup() {
+  [[ ${#DIRSTACK[@]} -gt 1 ]] && popd
+  git rev-parse --verify "${BRANCH_NAME}"
+  git checkout dev
+  if [[ $? == 0 ]]; then
+    git branch -D "${BRANCH_NAME}"
+  fi
+}
+
+trap _cleanup EXIT
+
+REF_HEAD_FILE=.cron/ref_head.txt
+VIRTUALENV=pyro-cron-27
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${DIR}"
 ref=$(<${REF_HEAD_FILE})
 source activate "${VIRTUALENV}"
 git checkout dev
 git pull upstream dev
+git fetch origin "${BRANCH_NAME}"
 git checkout "${BRANCH_NAME}"
-git pull origin "${BRANCH_NAME}"
 git rebase dev
 bash perf_test.sh "${ref}"
 if [[ $? -ne 0 ]]; then
