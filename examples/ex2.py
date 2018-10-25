db5aef2f3ea4c6b0374eb03e03f1085baa1ec6d7
Neeraj Pradhan <npradhan@uber.com>
2018-10-25 11:15:49 -0700

    stash

--- examples/ex2.py
+++ examples/ex2.py
@@ -2,36 +2,17 @@ import torch
 import torch.multiprocessing as mp
 
 
-N = 2
 DEVICE = "cpu"
 
 
-def fn(x):
-    x["in"] = x["out"]
-    x["out"] = x["out"] + 1
-    return x
-
-
-def stream():
-    x = {"in": torch.tensor(1., device=DEVICE), "out": torch.tensor(1., device=DEVICE)}
-    for _ in range(10):
-        x = fn(x)
-        yield x
-
-
-def proc(id, queue):
-    for x in stream():
-        queue.put((id, x))
+def proc(queue):
+    queue.put(torch.FloatTensor([1.], device=DEVICE))
 
 
 if __name__ == "__main__":
     ctx = mp.get_context("spawn")
-    queue = ctx.Manager().Queue()
-    procs = []
-    for i in range(N):
-        procs.append(ctx.Process(name=str(i), target=proc, args=(i, queue)))
-    for p in procs:
-        p.daemon = True
-        p.start()
-    for _ in range(N * 10):
-        print("received: ", queue.get())
+    queue = ctx.Queue()
+    p = ctx.Process(target=proc, args=(queue,))
+    p.daemon = True
+    p.start()
+    print("received: ", queue.get())
